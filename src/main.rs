46c07940b7dbfdadac7e296b5e5cf907a66af888
Wisdom Ogwu <iammadab@gmail.com>
2021-12-18 18:36:45 +0100

    Implemented value function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use rand::Rng;
 
 const NUM_OF_ACTIONS: usize = 3;
+const ROCK: usize = 0;
+const PAPER: usize = 1;
+const SCISSORS: usize = 2;
 
-fn main(){
+fn main() {
     let mut instance = RPS_CFR::new();
     let strategy = instance.getStrategy();
-    let action = instance.getAction(strategy);
+    let action = RPS_CFR::getAction(&strategy);
     println!("{:?}", action);
+    instance.train(10);
 }
 
 struct RPS_CFR {
@@ -52,7 +56,7 @@ impl RPS_CFR {
         self.strategy.clone()
     }
 
-    pub fn getAction(&mut self, strategy: Vec<f64>) -> usize {
+    pub fn getAction(strategy: &Vec<f64>) -> usize {
         let r: f64 = rand::thread_rng().gen();
         let mut action: usize = 0;
         let mut cummulativeProbability = 0.0;
@@ -65,4 +69,71 @@ impl RPS_CFR {
         }
         action
     }
+
+    pub fn train(&mut self, iterations: usize) {
+        let oppStrategy: Vec<f64> = vec![0.4, 0.3, 0.3];
+        let mut actionUtilities: Vec<f64> = vec![0.0, 0.0, 0.0];
+
+        for i in 0..iterations {
+            // Get regret matched mixed strategy actions
+            let strategy = self.getStrategy();
+            let myAction = RPS_CFR::getAction(&strategy);
+            let otherAction = RPS_CFR::getAction(&oppStrategy);
+
+            // What is this doing?
+            // Keeping constant the action of the other player
+            // Calculate what our utility would have been for all other actions
+            //
+            actionUtilities[otherAction] = 0.0;
+
+            if otherAction == NUM_OF_ACTIONS - 1 {
+                actionUtilities[0] = 1.0;
+            } else {
+                actionUtilities[otherAction + 1] = 1.0;
+            }
+
+            if otherAction == 0 {
+                actionUtilities[NUM_OF_ACTIONS - 1] = -1.0;
+            } else {
+                actionUtilities[otherAction - 1] = -1.0;
+            }
+
+            println!("{:?}", actionUtilities);
+
+            println!("Start");
+            println!("{}", myAction);
+            println!("{}", otherAction);
+            println!("End");
+            // Compute action utilities
+            // Accumulate action regrets
+        }
+    }
+
+    pub fn value(action1: usize, action2: usize) -> f64 {
+        if action1 == action2 {
+            return 0.0;
+        }
+        if ((action1 + 1) % 3) == action2 {
+            return -1.0;
+        }
+        return 1.0;
+    }
+}
+
+#[test]
+fn test_value_function() {
+    // Exhaust Rock
+    assert_eq!(RPS_CFR::value(ROCK, PAPER), -1.0);
+    assert_eq!(RPS_CFR::value(ROCK, SCISSORS), 1.0);
+    assert_eq!(RPS_CFR::value(ROCK, ROCK), 0.0);
+
+    // Exhaust Paper
+    assert_eq!(RPS_CFR::value(PAPER, PAPER), 0.0);
+    assert_eq!(RPS_CFR::value(PAPER, SCISSORS), -1.0);
+    assert_eq!(RPS_CFR::value(PAPER, ROCK), 1.0);
+
+    // Exhaust Scissors
+    assert_eq!(RPS_CFR::value(SCISSORS, PAPER), 1.0);
+    assert_eq!(RPS_CFR::value(SCISSORS, SCISSORS), 0.0);
+    assert_eq!(RPS_CFR::value(SCISSORS, ROCK), -1.0);
 }
